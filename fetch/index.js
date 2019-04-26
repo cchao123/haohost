fc770371f5aff0375f43159a0a1209eeff874665
v_chenchao <2020807070@qq.com>
2019-04-26 10:46:27 +0800

    add some file

--- fetch/index.js
+++ fetch/index.js
@@ -1,3 +1,51 @@
-export function fetch() {
-
+let $fetch = {
+  get(url, data) {
+    if (Object.prototype.toString.call(data) != "[object Object]") {
+      return {
+        then(callback) {
+          callback("GET请求入参格式不正确,需要传OBJECT")
+          return {
+            catch(err) {
+              err(new Error("入参格式错误"))
+            }
+          }
+        }
+      };
+    }
+    let queryString = "?"
+    for (let i in data) {
+      queryString += (i + "=" + data[i] + "&")
+    }
+    url = encodeURI(url + queryString.slice(0, -1))
+    return fetch(url, {
+      headers: {
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      credentials: "include"
+    }).then(res => res.json())
+  },
+  post(url, data) {
+    if (Object.prototype.toString.call(data) != "[object Object]") {
+      return {
+        then(callback) {
+          callback("POST请求入参格式不正确,需要传OBJECT")
+          return {
+            catch(err) {
+              err(new Error("入参格式错误"))
+            }
+          }
+        }
+      };
+    }
+    return fetch(url, {
+      body: JSON.stringify(data),//字符串
+      headers: {
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      credentials: "include",
+      method: "POST"
+    }).then(res => res.json())
+  },
 }
+
+export default $fetch
--- imgLoading/README.md
+++ imgLoading/README.md
@@ -1,9 +1,13 @@
-## h5 图片资源预加载
+## 图片资源预加载
 
-```
+```javascript
+
+// 引入
 import {imgLoading} from 'haohost/imgLoading'
+
+// 调用
 imgLoading(imgArr, (percentage) => {
 	console.log(percentage + '%')
 })
-```
 
+```
